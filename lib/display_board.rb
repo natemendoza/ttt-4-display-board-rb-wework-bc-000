e00a49de379fc98a5a5acea25d82f7dc329e06d7
Nathaniel Mendoza <nathaniel.mendoza@wework.com>
2018-02-14 00:50:49 +0000

    automatically backed up by learn

---
+++ lib/display_board.rb
@@ -0,0 +1,12 @@
+board = [" "," "," "," "," "," "," "," "," "]
+
+def display_board (board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "  
+end
+
+display_board
+
